e955b65c8ecf1fe9618437f7b0f576eb37e6eb5c
IbrohimRavs <124879713+IbrohimRavs@users.noreply.github.com>
2023-09-13 19:48:56 +0300

    Add files via upload

---
+++ 2-DARS/js/main.js
@@ -0,0 +1,31 @@
+// console.log("ok")
+// alert("ok")
+
+// let a = prompt("Name");
+// let b = prompt("Age");
+// let c = prompt("Gender");
+// let d = prompt("school");
+
+// console.log("Name", a);
+// console.log("Age", b);
+// console.log("Gender", c);
+// console.log("school", d);
+
+
+let a =  prompt("Ismingizni kiriting");
+let b = prompt("Yoshingizi kiriting");
+let c = prompt("6 + 3");
+let d = prompt("20 - 5");
+let x = prompt("15 * 2");
+let y = prompt("8 / 4");
+let j = prompt("10 % 3");
+
+
+console.log("Isim",a);
+console.log("Yosh",b);
+console.log(6 + 3," togri javob",c,"sizni javobingiz");
+console.log(20 - 5," togri javob",d,"sizni javobingiz");
+console.log(15 * 2," togri javob",x,"sizni javobingiz");
+console.log(8 / 4," togri javob",y,"sizni javobingiz");
+console.log(10 % 3," togri javob",j,"sizni javobingiz");
+
